a3daa67d7eb67095dbf2a19b86e447802812aba2
Patrick Ratzow <patrickratzow@hotmail.dk>
2022-04-17 18:14:21 +0200

    fix: remove unused files

--- Services/Inpark/Inpark.Contracts/OpeningHoursEvent.cs
+++
@@ -1,1 +0,0 @@
-namespace Zoo.Inpark.Contracts;
--- Services/Inpark/Inpark/Features/OpeningHours/Events/OpeningHoursUpdatedEvent.cs
+++
@@ -1,6 +0,0 @@
-using Zoo.Inpark.Common;
-using Zoo.Inpark.Entities;
-
-namespace Zoo.Inpark.Features.OpeningHours.Events;
-
-public record OpeningHoursUpdatedEvent(List<OpeningHour> OpeningHours) : DomainEvent;
