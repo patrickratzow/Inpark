ad48080b517ae5692dc7a946cd4361fa163d5501
Nicholas Wolf <nikolajcarlslund@gmail.com>
2022-04-17 21:30:30 +0200

    feat: missing tests

--- Services/Inpark/Inpark.Tests/DomainFixture/Configurations/Entities/AnimalConfiguration.cs
+++ Services/Inpark/Inpark.Tests/DomainFixture/Configurations/Entities/AnimalConfiguration.cs
@@ -7,5 +7,12 @@ public class AnimalConfiguration : FixtureConfiguration<Animal>
 {
     public override void Configure()
     {
+        Property(x => x.Category)
+            .Length(1).IsValid()
+            .Empty().IsInvalid();
+        
+        Property(x => x.Content)
+            .Empty().IsInvalid()
+            .Valid("[]");
     }
 }
