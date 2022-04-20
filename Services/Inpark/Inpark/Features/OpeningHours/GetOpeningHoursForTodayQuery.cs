431004bfb66d97264cd037cc0cbd8b996d2d3551
Patrick Ratzow <patrickratzow@hotmail.dk>
2022-04-20 23:39:39 +0200

    fix: don't compare selection in opening hours by year, month and days

--- Services/Inpark/Inpark/Features/OpeningHours/GetOpeningHoursForTodayQuery.cs
+++ Services/Inpark/Inpark/Features/OpeningHours/GetOpeningHoursForTodayQuery.cs
@@ -21,46 +21,38 @@ public class GetOpeningHoursForTodayQueryHandler
     public async Task<OneOf<List<OpeningHourDto>>> Handle(GetOpeningHoursForTodayQuery request,
             CancellationToken cancellationToken)
     {
-        var today = _clock.Today;
-        var day = new DateTime(today.Year, today.Month, today.Day);
+        var today = new DateOnly(_clock.Today.Year, _clock.Today.Month, _clock.Today.Day);
+        var value = (int)Enum.Parse(typeof(WeekDay), today.DayOfWeek.ToString());
         var openingHours = await _context.OpeningHours
             .Where(x => 
-                x.Range.Start.Year <= day.Year &&
-                x.Range.Start.Month <= day.Month &&
-                x.Range.Start.Day <= day.Day &&
-                x.Range.End.Year >= day.Year &&
-                x.Range.End.Month >= day.Month &&
-                x.Range.End.Day >= day.Day)
+                x.Range.End.Year >= today.Year &&
+                x.Range.End.Month >= today.Month &&
+                x.Range.End.Day >= today.Day)
+            .Where(x => ((int)x.Days & value) != 0)
             .ToListAsync(cancellationToken);
         
         // Finds the closest start point to today that is not in the future
-        var closest = openingHours
-            .GroupBy(x => new DateTime(x.Range.Start.Year, x.Range.Start.Month, x.Range.Start.Day))
-            .Select(x => new
+        return openingHours
+            .Where(o =>
             {
-                Date = x.Key,
-                Grouping = x,
-                DayDifference = (today - x.Key).TotalDays
+                var rangeStart = o.Range.Start;
+                var rangeEnd = o.Range.End;
+                var dateStart = new DateOnly(rangeStart.Year, rangeStart.Month, rangeStart.Day);
+                var dateEnd = new DateOnly(rangeEnd.Year, rangeEnd.Month, rangeEnd.Day);
+
+                // Today should be between dateStart & dateEnd
+                return dateStart <= today && dateEnd >= today;
             })
-            .Where(x => x.DayDifference >= 0)
-            .OrderBy(x => x.DayDifference)
-            .Select(x => x.Grouping)
-            .FirstOrDefault();
-        // Map to DTO
-        var result = closest?
+            .OrderByDescending(o => o.Range.Start)
+            .Take(1)
             .Select(x => new OpeningHourDto(
                 x.Name,
                 x.Range.Start,
                 x.Range.End,
                 x.Open,
                 x.Days.ToDays()
-            ));
-        // Filter out if day isn't today
-        var todayOpeningHours = result?
-            .Where(x => x.Days.Contains(today.DayOfWeek.ToString()))
+            ))
             .ToList();
-
-        return todayOpeningHours ?? new List<OpeningHourDto>();
     }
 }
 
