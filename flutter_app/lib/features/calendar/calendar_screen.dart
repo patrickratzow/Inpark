54990539cb18bba929f9f59d982c570f7dc250e4
Patrick Ratzow <patrickratzow@hotmail.dk>
2022-05-03 17:44:27 +0200

    feat: work on calendar

--- flutter_app/lib/features/calendar/calendar_screen.dart
+++ flutter_app/lib/features/calendar/calendar_screen.dart
@@ -8,13 +8,15 @@ import 'package:flutter_app/common/ui/screen_app_bar.dart';
 import 'package:flutter_app/common/ui/title_bar.dart';
 import 'package:flutter_app/routes.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:intl/date_symbols.dart';
+import 'package:intl/intl.dart';
 
 class CalendarScreen extends HookWidget {
   const CalendarScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var date = useState(CalendarDate(2022, 10, 1));
+
     return Scaffold(
       appBar: const ScreenAppBar(
         title: "Kalendar",
@@ -25,9 +27,9 @@ class CalendarScreen extends HookWidget {
             color: const Color(0xffeef2ee),
             child: Column(
               children: [
-                _buildHeaderRow(),
-                _buildDaysRow(),
-                _buildDates(),
+                _buildHeaderRow(date),
+                _buildDaysRow(date),
+                _buildDates(date),
               ],
             ),
           ),
@@ -36,7 +38,7 @@ class CalendarScreen extends HookWidget {
     );
   }
 
-  Widget _buildHeaderRow() {
+  Widget _buildHeaderRow(ValueNotifier<CalendarDate> date) {
     const double height = 48;
     const double width = 24;
     const double iconSize = 18;
@@ -50,10 +52,10 @@ class CalendarScreen extends HookWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Padding(
+        Padding(
           padding: EdgeInsets.fromLTRB(16, 20, 16, 12),
           child: Text(
-            "Oktober 2022",
+            date.value.monthName,
             style: TextStyle(
               fontSize: 20,
               height: 1,
@@ -90,8 +92,8 @@ class CalendarScreen extends HookWidget {
     );
   }
 
-  Widget _buildDaysRow() {
-    final days = ["Man", "Tir", "Ons", "Tor", "Fre", "Lør", "Søn"];
+  Widget _buildDaysRow(ValueNotifier<CalendarDate> date) {
+    final days = date.value.weekdays;
 
     return Padding(
       padding: const EdgeInsets.fromLTRB(16, 4, 16, 16),
@@ -112,9 +114,8 @@ class CalendarScreen extends HookWidget {
     );
   }
 
-  Widget _buildDates() {
-    var date = CalendarDate(2022, 10, 1);
-    var days = date.daysInMonth;
+  Widget _buildDates(ValueNotifier<CalendarDate> date) {
+    var days = date.value.daysInMonth;
     var amountOfRows = (days / 7).ceil();
     var rows = <Row>[];
     for (var i = 0; i < amountOfRows; i++) {
@@ -138,7 +139,7 @@ class CalendarScreen extends HookWidget {
               ),
               child: Center(
                 child: Text(
-                  date.day.toString(),
+                  (1 + ((i + 1) * j)).toString(),
                   style: const TextStyle(
                     fontSize: 16,
                     color: Color(0xff0A0A0A),
@@ -157,6 +158,15 @@ class CalendarScreen extends HookWidget {
   }
 }
 
+class CalendarBody extends StatelessWidget {
+  const CalendarBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 class CalendarDate {
   int year;
   int month;
@@ -164,13 +174,13 @@ class CalendarDate {
 
   CalendarDate(this.year, this.month, this.day);
 
+  DateTime get date => DateTime(year, month, day);
   int get daysInMonth => difference(1).inDays;
   int get daysInPreviousMonth => difference(-1).inDays;
 
   Duration difference(int months) {
     assert(months != 0);
 
-    var current = DateTime(year, month);
     DateTime next;
     if (months > 0) {
       // If the month is december go to next year
@@ -183,6 +193,37 @@ class CalendarDate {
           : DateTime(year, month - 1);
     }
 
-    return next.toUtc().difference(current);
+    return next.toUtc().difference(date);
+  }
+
+  DateFormat _formatter(String format) => DateFormat(format);
+
+  String get monthName => _formatter("MMMM yyyy").format(date).toString();
+
+  List<String> get weekdays {
+    List<String> days = [];
+
+    var date = DateTime(2022, 05, 02);
+    for (var i = 0; i < 7; i++) {
+      var abbreviation =
+          _formatter(DateFormat.ABBR_WEEKDAY).format(date).toString();
+      days.add(abbreviation);
+
+      date = date.add(const Duration(days: 1));
+    }
+
+    return days;
+  }
+
+  // Check if an offset based on UI rows for calendar is inside the current month
+  // Outside elements will be considered part of last or next month
+  // Base it off weekday, so that if previous month's 31st is a Tuesday, and 1st for the current month is 1st, an offset of 0 would not be current month
+  bool isInsideMonth(int offset) {
+    var date = DateTime(year, month, 1);
+    var weekday = date.weekday;
+    var offsetWeekday = (weekday + offset) % 7;
+    var offsetDay = (offsetWeekday + 1) % 7;
+
+    return offsetDay != 0;
   }
 }
