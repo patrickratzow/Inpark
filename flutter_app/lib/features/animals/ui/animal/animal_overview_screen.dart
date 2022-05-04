36fdeb64a43085f4ae67290c2fcc27bde07b7651
Nicholas Wolf <nikolajcarlslund@gmail.com>
2022-05-04 09:22:52 +0200

    fix: quotations

--- flutter_app/lib/features/animals/ui/animal/animal_overview_screen.dart
+++ flutter_app/lib/features/animals/ui/animal/animal_overview_screen.dart
@@ -2,8 +2,8 @@ import "package:flutter/material.dart";
 import "package:flutter_app/common/ui/screen_app_bar.dart";
 import "package:flutter_app/features/animals/models/animals_model.dart";
 import "package:flutter_app/features/animals/ui/search_bar.dart";
-import 'package:flutter_app/features/calendar/calendar_screen.dart';
-import 'package:flutter_app/routes.dart';
+import "package:flutter_app/features/calendar/calendar_screen.dart";
+import "package:flutter_app/routes.dart";
 import "package:provider/provider.dart";
 
 import "animal_card.dart";
