e761d77768d672c247b5e7eb8f7b0d1aa2da0286
Nicholas Wolf <nikolajcarlslund@gmail.com>
2022-05-23 11:00:39 +0200

    fix: unused animal provider variable

--- flutter_app/lib/features/animals/ui/animal/animal_map.dart
+++ flutter_app/lib/features/animals/ui/animal/animal_map.dart
@@ -2,8 +2,6 @@ import "dart:math";
 
 import "package:flutter/material.dart";
 import "package:flutter_app/features/animals/models/animal_area.dart";
-import "package:flutter_app/features/animals/models/animals_model.dart";
-import "package:flutter_app/hooks/hooks.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:flutter_svg/flutter_svg.dart";
 
@@ -17,7 +15,6 @@ class AnimalMap extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final animal = useProvider<AnimalsModel>(watch: true);
     final width = MediaQuery.of(context).size.width;
 
     return InteractiveViewer(
