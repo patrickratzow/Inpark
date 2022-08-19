2542553bce03fea98cd17bcc6615d4e846cfe628
Patrick Ratzow <pra@commentor.dk>
2022-08-19 15:54:25 +0200

    feat: target prod azure functions

--- Services/Maps/Maps.Tests.Load/Program.cs
+++ Services/Maps/Maps.Tests.Load/Program.cs
@@ -1,54 +1,49 @@
 ﻿using System.Net;
 using System.Text;
 using NBomber.CSharp;
-using NBomber.Plugins.Http.CSharp;
-using NBomber.Plugins.Network.Ping;
 using System.Text.Json;
-using NBomber.Plugins.Http.FSharp;
-using Http = NBomber.Plugins.Http.CSharp.Http;
+using NBomber.Plugins.Network.Ping;
 using HttpClientFactory = NBomber.Plugins.Http.CSharp.HttpClientFactory;
 using Response = NBomber.Contracts.Response;
 
 var random = new Random();
-var step = Step.Create(
-    "fetch_html_page",
-    clientFactory: HttpClientFactory.Create(),
-    execute: context =>
+
+var httpFactory = HttpClientFactory.Create();
+var step = Step.Create("step", httpFactory, async context =>
+{
+    var data = new
     {
-        var data = new
-        {
-            Latitude = -90 + (random.NextDouble() * 180),
-            Longitude = -180 + (random.NextDouble() * 360)
-        };
-        var jsonData = JsonSerializer.Serialize(data, new JsonSerializerOptions()
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-        });
-        var body = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        Latitude = -90 + (random.NextDouble() * 180),
+        Longitude = -180 + (random.NextDouble() * 360)
+    };
+    var jsonData = JsonSerializer.Serialize(data, new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    });
+    var body = new StringContent(jsonData, Encoding.UTF8, "application/json");
+    var response =
+        await context.Client.PostAsync(
+            "https://func-zeta-maps-northeurope-dev-01.azurewebsites.net/api/ping-location",
+            body);
 
-        var request = Http.CreateRequest("POST", "http://localhost:7071/api/ping-location")
-            .WithBody(body)
-            .WithCheck(async (response) =>
-                response.StatusCode == HttpStatusCode.NoContent 
-                    ? Response.Ok() 
-                    : Response.Fail()
-                );
-
-        return Http.Send(request, context);
-    }
-);
-
+    return response.StatusCode == HttpStatusCode.NoContent
+        ? Response.Ok()
+        : Response.Fail(statusCode: (int)response.StatusCode);
+});
+            
+// second, we add our step to the scenario
 var scenario = ScenarioBuilder
-    .CreateScenario("simple_http", step)
+    .CreateScenario("maps_ping", step)
     .WithWarmUpDuration(TimeSpan.FromSeconds(5))
     .WithLoadSimulations(
-        Simulation.InjectPerSec(rate: 100, during: TimeSpan.FromSeconds(30))
+        Simulation.InjectPerSec(rate: 500, during: TimeSpan.FromSeconds(30))
     );
 
 // creates ping plugin that brings additional reporting data
-var pingPluginConfig = PingPluginConfig.CreateDefault(new[] { "localhost:7252" });
+var pingPluginConfig = PingPluginConfig.CreateDefault(new[] { "func-zeta-maps-northeurope-dev-01.azurewebsites.net" });
 var pingPlugin = new PingPlugin(pingPluginConfig);
 
 NBomberRunner
     .RegisterScenarios(scenario)
+    .WithWorkerPlugins(pingPlugin)
     .Run();
