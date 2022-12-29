9bd6dba109c0159f17e8c30f6773924a7b87803a
Patrick Ratzow <patrickratzow@hotmail.dk>
2022-12-30 00:15:14 +0100

    fix(shop): add missing var

--- Services/Food/Food.Tests/Features/Shops/CreateShopTests.cs
+++ Services/Food/Food.Tests/Features/Shops/CreateShopTests.cs
@@ -12,52 +12,53 @@ namespace Zeta.Inpark.Food.Tests.Features.Shops;
 [TestFixture]
 public class CreateShopTests : TestBase
 {
-    [Test]
-    public async Task CreateShop_WhenMerchant_Exists()
-    {
-        // Arrange
-        var merchant = await Create<Merchant>();
-        var command = new Faker<CreateShop.Command>()
-            .CustomInstantiator(f => new(
-                Guid.NewGuid(),
-                merchant.Id,
-                f.Company.CompanyName(),
-                f.Lorem.Paragraph()
-            ))
-            .Generate();
-        
-        // Act
-        var result = await Send(command);
+  [Test]
+  public async Task CreateShop_WhenMerchant_Exists()
+  {
+    // Arrange
+    var merchant = await Create<Merchant>();
+    var command = new Faker<CreateShop.Command>()
+        .CustomInstantiator(f => new(
+            Guid.NewGuid(),
+            merchant.Id,
+            f.Company.CompanyName(),
+            f.Lorem.Paragraph()
+        ))
+        .Generate();
 
-        // Assert
-        result.Should().BeOfType<Unit>();
-        await ShouldFind<Merchant>(command.Id);
-    }
-    
-    [Test]
-    public async Task CvrNumberAlreadyExists_WhenCvrNumber_IsInUse()
-    {
-        // Arrange
-        await Add(
-            Fixture.Valid<Merchant>()
-                .With(x => x.CvrNumber, ValueObjects.CvrNumber.From(CvrNumber))
-                .Create()
-        );
-        var command = new Faker<CreateMerchant.Command>()
-            .CustomInstantiator(f => new(
-                Guid.NewGuid(),
-                f.Company.CompanyName(),
-                CvrNumber
-            ))
-            .Generate();
-        
-        // Act
-        var result = await Send(command);
+    // Act
+    var result = await Send(command);
 
-        // Assert
-        var response = result.Should().BeOfType<CreateMerchant.CvrNumberAlreadyExists>();
-        response.CvrNumber.Should().Be(CvrNumber);
-        var count = await Count<Merchant>();
-        count.Should().Be(1);
-    }
+    // Assert
+    result.Should().BeOfType<Unit>();
+    await ShouldFind<Merchant>(command.Id);
+  }
+
+  [Test]
+  public async Task CvrNumberAlreadyExists_WhenCvrNumber_IsInUse()
+  {
+    const string CvrNumber = "12345678";
+    // Arrange
+    await Add(
+        Fixture.Valid<Merchant>()
+            .With(x => x.CvrNumber, ValueObjects.CvrNumber.From(CvrNumber))
+            .Create()
+    );
+    var command = new Faker<CreateMerchant.Command>()
+        .CustomInstantiator(f => new(
+            Guid.NewGuid(),
+            f.Company.CompanyName(),
+            CvrNumber
+        ))
+        .Generate();
+
+    // Act
+    var result = await Send(command);
+
+    // Assert
+    var response = result.Should().BeOfType<CreateMerchant.CvrNumberAlreadyExists>();
+    response.CvrNumber.Should().Be(CvrNumber);
+    var count = await Count<Merchant>();
+    count.Should().Be(1);
+  }
 }
